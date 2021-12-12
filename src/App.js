c9b6c866b4ba50fb09e8ab9c367e2a4546bd7f79
Arpit Verma <arpitv970@gmail.com>
2021-12-13 01:04:41 +0530

    Added `TinderCard` component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import TinderCards from './TinderCards';
 
 function App() {
   return (
     // BEM class naming convention
     <div className="app">
       <Header />
-      {/* TinderCards */}
+      <TinderCards />
       {/* SwipeButtons */}
     </div>
   );
