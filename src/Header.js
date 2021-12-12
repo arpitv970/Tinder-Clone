ae2d94dc7fe32a4dd0fa347dc494682345e7baec
Arpit Verma <arpitv970@gmail.com>
2021-12-12 17:13:35 +0530

    Added icons and logo - Profile icon - Tinder logo - Chats icon

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Header.css'
+import { IconButton } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
 
 function Header() {
     return (
         <div className='header'>
-            <h2>Am Header!</h2>
+            <IconButton>
+                <PersonIcon fontSize='large' className='header_icon' />
+            </IconButton>
+
+            <img className='header_logo' src='https://1000logos.net/wp-content/uploads/2018/07/Tinder-icon.png' alt='Tinder Logo' />
+
+            <IconButton>
+                <ForumIcon fontSize='large' className='header_icon'/>
+            </IconButton>
         </div>
     )
 }
