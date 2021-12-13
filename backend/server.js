048afacf32e7250ce537ec833508eb9f7e7110bd
Arpit Verma <arpitv970@gmail.com>
2021-12-13 18:55:19 +0530

    Connection created b/w backend and database

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,48 @@
 import express from "express";
 import mongoose from "mongoose";
+import Cors from "cors";
+
+import Cards from "./dbCards.js";
 
 // App Config
 const app = express();
 const port = process.env.PORT || 8001
+const connection_url = 'mongodb+srv://admin:admin@cluster0.zcdlu.mongodb.net/tinderDB?retryWrites=true&w=majority'
 
 // Middlewares
+app.use(express.json());
+app.use(Cors());
 
 // DB Config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 // API Endpoints
 app.get('/', (req, res) => res.status(200).send("Hello World!!!"));
+
+app.post('/tinder/cards', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get('/tinder/cards', (req, res) => {
+    Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
 
 // Listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
