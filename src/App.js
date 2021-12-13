5f2923701286dbf5248b1f22f1dbf6b1e3164619
Arpit Verma <arpitv970@gmail.com>
2021-12-13 14:07:59 +0530

    Added `SwipeButtons` component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import SwipeButtons from './SwipeButtons';
 import TinderCards from './TinderCards';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <div className="app">
       <Header />
       <TinderCards />
-      {/* SwipeButtons */}
+      <SwipeButtons />
     </div>
   );
 }
