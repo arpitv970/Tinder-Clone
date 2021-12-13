3a4f18fc4cf6c8f1a6ba09a6a4837172577b78ac
Arpit Verma <arpitv970@gmail.com>
2021-12-13 20:05:59 +0530

    Added feature to fetch data from database

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import TinderCard from 'react-tinder-card';
 import './TinderCards.css'
+import axios from './axios.js';
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-            name: 'Tsunade Senju',
-            url: "https://i.pinimg.com/originals/bb/85/d8/bb85d8903c0d5d963bd26b17bbaf2f90.jpg"
-        },
-        {
-            name: 'Fubuki',
-            url: "https://wallpaperaccess.com/full/1620801.png"
-        },
-    ]);
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get('/tinder/cards');
+
+            setPeople(req.data);
+        }
+
+        fetchData();
+    }, []);
+
+    console.log(people);
 
     const swiped = (direction, nameToDelete) => {
         console.log('removing: ' + nameToDelete);
@@ -28,7 +32,7 @@ function TinderCards() {
             <div className="tinderCards__cardContainer">
                 {people.map(person => (
                     <TinderCard className='swipe' key={person.name} preventSwipe={['up', 'down']} onSwipe={(dir) => swiped(dir, person.name)} onCardLeftScreen={() => outOfFrame(person.name)}>
-                        <div style={{backgroundImage: `url(${person.url})`}} className='card'>
+                        <div style={{backgroundImage: `url(${person.imgUrl})`}} className='card'>
                             <h3>{person.name}</h3>
                         </div>
                     </TinderCard>
